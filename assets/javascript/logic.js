bd8c6de93031ba7af91fc13805c8c5649893b7fa
izuua <izuuaaf@gmail.com>
2019-10-16 18:02:55 -0400

    Updated ul css to be vw based.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,3 +12,25 @@ for (i = 0; i < coll.length; i++) {
         }
     });
 }
+
+// Just testing some resizing code for potential future use
+// function resize() {
+//     let screenSize = screen.width;
+
+//     var buttons = document.querySelectorAll('button');
+
+//     for (let i = 0; i < buttons.length; i++) {
+//         let thisButton = buttons[i];
+//         console.log(thisButton.textContent.length);
+//         if (screen.width < 435 && thisButton.textContent.length > 40) {
+//             thisButton.setAttribute("style", "font-size: 10px")
+//         }
+//     }
+// }
+
+// resize(); //make sure the function runs on page load
+
+// window.onresize = resize; //reruns the function anytime the window is resized
+
+
+
